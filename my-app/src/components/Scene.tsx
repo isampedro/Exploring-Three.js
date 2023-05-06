f6c7bbc9892300a9cf7ae75b75a5c4c20a78b00a
Ignacio <isampedro@itba.edu.ar>
2023-05-06 01:31:35 -0300

    [Refactor] Separated code into blocks

--- my-app/src/components/Scene.tsx
+++ my-app/src/components/Scene.tsx
@@ -5,18 +5,25 @@ import {GUI} from "dat.gui";
 
 const Scene = () => {
     const sceneRef = useRef<HTMLDivElement>(null);
-    const [arcBallEnabled, setArcBallEnabled] = useState(true);
-    const [cylinderRadius, setCylinderRadius] = useState(1)
+    const Size = 500, Divisions = 50;
+    const [arcBallEnabled, setArcBallEnabled] = useState<boolean>(true);
+    const [cylinderRadius, setCylinderRadius] = useState<number>(1);
+    const [seePlane, setSeePlane] = useState<boolean>(false);
+    const [gridEnabled, setGridEnabled] = useState<boolean>(true);
 
-    const createSpotlight = (color: number) => {
+    const createSpotlight = (color: number, y: number) => {
         const newObj = new THREE.SpotLight(color, 100);
         newObj.castShadow = true;
         newObj.angle = 0.3;
         newObj.penumbra = 0.2;
         newObj.decay = 2;
-        newObj.position.x = 100;
+        newObj.position.x = 300;
         newObj.position.z = 50;
         newObj.castShadow = true;
+        newObj.position.y = y;
+        newObj.target.position.set(25, y, 20);
+        newObj.target.updateMatrixWorld();
+        newObj.lookAt(newObj.target.position);
         return newObj;
     };
 
@@ -30,7 +37,7 @@ const Scene = () => {
     };
 
     const createPlane = () => {
-        const geometry = new THREE.PlaneGeometry(2000, 2000);
+        const geometry = new THREE.PlaneGeometry(200, 500);
         const material = new THREE.MeshBasicMaterial({color: 0x000000});
         return new THREE.Mesh(geometry, material);
     };
@@ -44,7 +51,7 @@ const Scene = () => {
     };
 
     const createCylinder = () => {
-        const cylinderGeometry = new THREE.CylinderGeometry( cylinderRadius, cylinderRadius, 1000, 32 );
+        const cylinderGeometry = new THREE.CylinderGeometry( cylinderRadius, cylinderRadius, 500, 32 );
         const cylinderMaterial = new THREE.MeshStandardMaterial( {color: 0x808080} );
         const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
         cylinder.position.set(0, 0, 0);
@@ -52,98 +59,91 @@ const Scene = () => {
         return cylinder;
     }
 
-    useEffect(() => {
-        const scene = new THREE.Scene();
-        const renderer = createRenderer();
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap;// RENDERER
-        sceneRef.current?.appendChild(renderer.domElement);
-        const plane = createPlane();                            // PLANE
-        scene.add(plane);
-        const camera = createCamera();                          // CAMERA
+    const createScene = () => {
+        // @ts-ignore
+        let scene = {
+            scene: new THREE.Scene(),
+            renderer: createRenderer(),
+            plane: createPlane(),
+            camera: createCamera(),
+            cylinder: createCylinder(),
+            gridHelper: new THREE.GridHelper( Size, Divisions ),
+            spotLight1: createSpotlight(0xFF0000, 80),
+            spotLight2: createSpotlight(0x00FF00, 0),
+            spotLight3: createSpotlight(0x0000FF, -80),
+            gui: new GUI(),
+        }
+        return {...scene, ...{
+                controlsFolder: scene.gui.addFolder("Controls"),
+                controls: new ArcballControls(scene.camera, scene.renderer.domElement, scene.scene),
+                axesHelper: new THREE.AxesHelper(1),
+                lightHelper1: new THREE.SpotLightHelper(scene.spotLight1),
+                lightHelper2: new THREE.SpotLightHelper(scene.spotLight2),
+                lightHelper3: new THREE.SpotLightHelper(scene.spotLight3),
+            }};
+    }
 
-        const cylinder = createCylinder();
-        scene.add( cylinder );
-
-        // To rotate scene
-        const controls = new ArcballControls(camera, renderer.domElement, scene);
-
-        // GUI controls
-        const gui = new GUI();
-
-        // SPOTLIGHT #1
-        const spotLight1 = createSpotlight(0xFF0000);
-        spotLight1.position.y = 60;
-        spotLight1.target.position.set(25, 60, 20);
-        spotLight1.target.updateMatrixWorld(); // update the spotlight's target matrix
-        spotLight1.lookAt(spotLight1.target.position); // make the spotlight look at the target
-        // SPOTLIGHT #2
-
-        const spotLight2 = createSpotlight(0x00FF00);
-        spotLight2.target.position.set(25, 0, 20);
-        spotLight2.target.updateMatrixWorld(); // update the spotlight's target matrix
-        spotLight2.lookAt(spotLight2.target.position); // make the spotlight look at the target
-        // SPOTLIGHT #3
-        const spotLight3 = createSpotlight(0x0000FF);
-        spotLight3.position.y = -60;
-        spotLight3.target.position.set(25, -60, 20);
-        spotLight3.target.updateMatrixWorld(); // update the spotlight's target matrix
-        spotLight3.lookAt(spotLight2.target.position); // make the spotlight look at the target
-
-        // GUI folders
-        const controlsFolder = gui.addFolder("Controls");
-
-        // ADD spotlights colors variation AND spotlights angle variation
-        controlsFolder.addColor(spotLight1, "color").onChange(() => renderer.render(scene, camera));
-        controlsFolder.add({'spotlight 1 angle': spotLight1.angle}, 'spotlight 1 angle', 0, Math.PI/4).onChange((value) => {
-            spotLight1.angle = value;
-            renderer.render(scene, camera);
+    const setupControlsGUI = (wholeScene: any) => {
+        wholeScene.controlsFolder.addColor(wholeScene.spotLight1, "color").onChange(() => wholeScene.renderer.render(wholeScene.scene, wholeScene.camera));
+        wholeScene.controlsFolder.add({'spotlight 1 angle': wholeScene.spotLight1.angle}, 'spotlight 1 angle', 0, Math.PI/4).onChange((value: number) => {
+            wholeScene.spotLight1.angle = value;
+            wholeScene.renderer.render(wholeScene.scene, wholeScene.camera);
         });
-        controlsFolder.addColor(spotLight2, "color").onChange(() => renderer.render(scene, camera));
-        controlsFolder.add({'spotlight 2 angle': spotLight2.angle}, 'spotlight 2 angle', 0, Math.PI/4).onChange((value) => {
-            spotLight2.angle = value;
-            renderer.render(scene, camera);
+        wholeScene.controlsFolder.addColor(wholeScene.spotLight2, "color").onChange(() => wholeScene.renderer.render(wholeScene.scene, wholeScene.camera));
+        wholeScene.controlsFolder.add({'spotlight 2 angle': wholeScene.spotLight2.angle}, 'spotlight 2 angle', 0, Math.PI/4).onChange((value: number) => {
+            wholeScene.spotLight2.angle = value;
+            wholeScene.renderer.render(wholeScene.scene, wholeScene.camera);
         });
-        controlsFolder.addColor(spotLight3, "color").onChange(() => renderer.render(scene, camera));
-        controlsFolder.add({'spotlight 3 angle': spotLight1.angle}, 'spotlight 3 angle', 0, Math.PI/4).onChange((value) => {
-            spotLight3.angle = value;
-            renderer.render(scene, camera);
+        wholeScene.controlsFolder.addColor(wholeScene.spotLight3, "color").onChange(() => wholeScene.renderer.render(wholeScene.scene, wholeScene.camera));
+        wholeScene.controlsFolder.add({'spotlight 3 angle': wholeScene.spotLight1.angle}, 'spotlight 3 angle', 0, Math.PI/4).onChange((value: number) => {
+            wholeScene.spotLight3.angle = value;
+            wholeScene.renderer.render(wholeScene.scene, wholeScene.camera);
         });
-        controlsFolder.add({arcBallEnabled}, "arcBallEnabled").onChange((value) => {
-            setArcBallEnabled(value);
-            controls.enabled = value;
-        });
-        controlsFolder.add({cylinderRadius}, "cylinderRadius").onChange((value) => {
+        wholeScene.controlsFolder.add({cylinderRadius}, "cylinderRadius").onChange((value: number) => {
             setCylinderRadius(value);
-            cylinder.geometry.dispose();
-            cylinder.geometry = new THREE.CylinderGeometry(value, value, 1000, 32);
+            wholeScene.cylinder.geometry.dispose();
+            wholeScene.cylinder.geometry = new THREE.CylinderGeometry(value, value, 1000, 32);
         });
-        controlsFolder.open();
-
-        scene.add(spotLight1, spotLight2, spotLight3);
-
-        controls.addEventListener("change", function () {
-            renderer.render(scene, camera);
+        wholeScene.controlsFolder.add({arcBallEnabled}, "arcBallEnabled").onChange((value: boolean) => {
+            setArcBallEnabled(value);
+            wholeScene.controls.enabled = value;
         });
+        wholeScene.controlsFolder.add({ seePlane }, 'seePlane').onChange((value: boolean) => {
+            setSeePlane(value);
+            wholeScene.plane.material.dispose();
+            wholeScene.plane.material = value ? new THREE.MeshPhongMaterial({color: 0x808080}): new THREE.MeshBasicMaterial({color: 0x000000});
+            wholeScene.plane.castShadow = value;
+        });
+        wholeScene.controlsFolder.add({ gridEnabled }, 'gridEnabled').onChange((value: boolean) => {
+            setGridEnabled(value);
+            wholeScene.gridHelper.visible = value;
+        });
+        wholeScene.controlsFolder.open();
+        wholeScene.controls.addEventListener("change", function () {
+            wholeScene.renderer.render(wholeScene.scene, wholeScene.camera);
+        });
+    }
 
-        // HELPERS
-        //  |   AXES
-        const axesHelper = new THREE.AxesHelper(5);
-        scene.add(axesHelper);
-        //  |   SPOTLIGHT
-        const lightHelper1 = new THREE.SpotLightHelper(spotLight1);
-        const lightHelper2 = new THREE.SpotLightHelper(spotLight2);
-        const lightHelper3 = new THREE.SpotLightHelper(spotLight3);
-        scene.add(lightHelper1, lightHelper2, lightHelper3);
+    useEffect(() => {
+        const wholeScene = createScene();
+
+        wholeScene.renderer.shadowMap.enabled = true;
+        wholeScene.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        sceneRef.current?.appendChild(wholeScene.renderer.domElement);
+
+        wholeScene.gridHelper.rotation.x = Math.PI / 2;
+        setupControlsGUI(wholeScene);
+        wholeScene.scene.add(wholeScene.plane, wholeScene.cylinder, wholeScene.gridHelper, wholeScene.spotLight3,
+            wholeScene.spotLight2, wholeScene.spotLight1, wholeScene.axesHelper, wholeScene.lightHelper3, wholeScene.lightHelper2, wholeScene.lightHelper1);
 
         const animate = () => {
             if (arcBallEnabled) {
-                controls.update();
+                wholeScene.controls.update();
             }
-            if (lightHelper1) lightHelper1.update();
-            if (lightHelper2) lightHelper2.update();
-            if (lightHelper3) lightHelper3.update();
-            renderer.render(scene, camera);
+            if (wholeScene.lightHelper1) wholeScene.lightHelper1.update();
+            if (wholeScene.lightHelper2) wholeScene.lightHelper2.update();
+            if (wholeScene.lightHelper3) wholeScene.lightHelper3.update();
+            wholeScene.renderer.render(wholeScene.scene, wholeScene.camera);
             requestAnimationFrame(animate);
         };
 
